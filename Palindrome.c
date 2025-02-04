fc6bc547e905577848ec236c2d92c7ea56e35e83
abhishek <aailabeu@gmail.com>
2025-02-04 09:12:58 +0530

    formatted

--- Palindrome.c
+++ Palindrome.c
@@ -1,21 +1,23 @@
-#include <stdio.h>  
+#include <stdio.h>
+int main()
+{
+    int num, originalNum, rev = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    originalNum = num;
+    while (num > 0)
+    {
+        rev = (rev * 10) + (num % 10);
+        num /= 10;
+    }
+    if (rev == originalNum)
+    {
+        printf("Palindrome\n");
+    }
+    else
+    {
+        printf("Not a Palindrome\n");
+    }
 
-int main() {  
-    int num, originalNum, rev = 0;  
-    printf("Enter a number: ");  
-    scanf("%d", &num);  
-    
-    originalNum = num; 
-    
-    while (num > 0) {  
-        rev = (rev * 10) + (num % 10);  
-        num /= 10; 
-    }  
-    if (rev == originalNum) {  
-        printf("Palindrome\n");  
-    } else {  
-        printf("Not a Palindrome\n");  
-    }  
-    
-    return 0;  
+    return 0;
 }
